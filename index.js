abfa24a6381dc5b3213f5e6400e19633e127e54c
tharindu99 <madushanka.best@gmail.com>
2017-11-17 11:45:35 +0530

    add meterial design

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('metlook/index');
 });
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
