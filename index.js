b00b887fef062558476bcf954e49ee9a548ba59e
tharindu99 <madushanka.best@gmail.com>
2017-11-21 14:58:25 +0530

    menu slidebar modified

--- index.js
+++ index.js
@@ -10,15 +10,20 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
+  
   var baseFrame = 
     {title: 'Web Site Owner',
-     userName: 'Tharindu Madushanka',
+     userName: 'Tharindu',
      siteName: ['AB.lk','bc.lk'],
      userImage:'images/user.png'
-    
     };
+  var sideMenu =
+    [
+      {name:'Home',icon:'home',link:'Home',state:'active'},
+      {name:'RegisteAd-block',icon:'layers',link:'reg_adblock'}
+    ]
 
-  response.render('metlook/index',baseFrame);
+  response.render('metlook/index',{baseFrame:baseFrame,sideMenu:sideMenu});
 });
 
 
