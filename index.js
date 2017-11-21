6abfddcd07e3af022451cd850b7d428fdc358947
tharindu99 <madushanka.best@gmail.com>
2017-11-21 16:36:25 +0530

    index page added

--- index.js
+++ index.js
@@ -12,6 +12,24 @@ app.set('view engine', 'ejs');
 app.get('/', function(request, response) {
   
   var baseFrame = 
+    {title: '',
+     userName: '',
+     siteName: [],
+     userImage:''
+    };
+  var sideMenu =
+    [
+      {name:'About',icon:'home',link:'about',state:'active'},
+      {name:'Concept',icon:'layers',link:'concept'},
+      {name:'Legator',icon:'layers',link:'legator'}
+    ]
+
+  response.render('metlook/index',{baseFrame:baseFrame,sideMenu:sideMenu});
+});
+
+app.get('/webSiteOwner', function(request, response) {
+  
+  var baseFrame = 
     {title: 'Web Site Owner',
      userName: 'Tharindu',
      siteName: ['AB.lk','bc.lk'],
@@ -20,12 +38,12 @@ app.get('/', function(request, response) {
   var sideMenu =
     [
       {name:'Home',icon:'home',link:'Home',state:'active'},
-      {name:'RegisteAd-block',icon:'layers',link:'reg_adblock'}
+      {name:'RegisteAd-block',icon:'layers',link:'reg_adblock'},
+      {name:'Earnings',icon:'monetization_on',link:'earnings'}
     ]
 
   response.render('metlook/index',{baseFrame:baseFrame,sideMenu:sideMenu});
 });
-
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
